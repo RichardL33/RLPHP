c53e26a830b67cd0cf0774f499d617c7065bc332
RichardL33 <BruceLeeroy@Bruce-Leeroy.local>
2014-12-11 17:45:24 -0500

    Move

--- Final Project/comments.php
+++
@@ -1,7 +0,0 @@
-<form action="post_comments.php" method="POST">
-	<textarea name="comment" id="" rows="4" cols="50" placeholder="What's up...."></textarea>
-
-	<div class="comment_box">
-		<input type="submit" value="POST">
-	</div>
-</form>
